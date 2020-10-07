05365dbeef4fa085b5ebb2160ee490de99f2c224
Yesenia Morales <170151@upslp.edu.mx>
2020-10-06 20:53:18 -0500

    170151

--- afd/afd_170151/target/debug/afd_170151.d
+++
@@ -1,1 +0,0 @@
-C:\Users\yesco\Documents\170151\afd\afd_170151\target\debug\afd_170151.exe: C:\Users\yesco\Documents\170151\afd\afd_170151\src\main.rs
--- afd/afd_170151/target/debug/deps/afd_170151.d
+++
@@ -1,5 +0,0 @@
-C:\Users\yesco\Documents\170151\afd\afd_170151\target\debug\deps\afd_170151.exe: src\main.rs
-
-C:\Users\yesco\Documents\170151\afd\afd_170151\target\debug\deps\afd_170151.d: src\main.rs
-
-src\main.rs:
