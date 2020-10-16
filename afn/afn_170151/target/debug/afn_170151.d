6eca649bfc81bb4a6f45ae76c251fd7b8f9826a9
Yesenia Morales <170151@upslp.edu.mx>
2020-10-16 09:06:30 -0500

    170151

--- afn/afn_170151/target/debug/afn_170151.d
+++
@@ -1,1 +0,0 @@
-D:\yesco\Documents\SEPTIMO\170151\afn\afn_170151\target\debug\afn_170151.exe: D:\yesco\Documents\SEPTIMO\170151\afn\afn_170151\src\main.rs
--- afn/afn_170151/target/debug/deps/afn_170151.d
+++
@@ -1,5 +0,0 @@
-D:\yesco\Documents\SEPTIMO\170151\afn\afn_170151\target\debug\deps\afn_170151.exe: src\main.rs
-
-D:\yesco\Documents\SEPTIMO\170151\afn\afn_170151\target\debug\deps\afn_170151.d: src\main.rs
-
-src\main.rs:
